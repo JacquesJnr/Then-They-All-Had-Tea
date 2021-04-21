e66f7b4c102f96d70ff46b35808fdf08d9f04f64
Jacques Visser <jacquesvisserjnr@gmail.com>
2021-04-20 23:38:35 -0700

    Implemented Day/ Night Cycle

--- Assets/Scripts/DayNight.cs
+++ Assets/Scripts/DayNight.cs
@@ -1,9 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DayNight : MonoBehaviour
 {
+    const float timer = 1f;
+    [Range(0, timer)] public float elapsed = 0;
+    [SerializeField] private CanvasGroup BG;
+    [SerializeField] private bool dayTime = true;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,25 @@ public class DayNight : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        elapsed += Time.deltaTime;
+
+        if(elapsed >= timer)
+        {
+            FadeInOut(dayTime);
+            Debug.Log("Time");
+            if (dayTime)
+                dayTime = false;
+            else
+                dayTime = true;
+            elapsed = 0;
+        }
+    }
+
+    public void FadeInOut(bool isDay)
+    {
+        if(isDay)
+            BG.LeanAlpha(0, 0.5f);
+        else
+            BG.LeanAlpha(1, 0.5f);
     }
 }
--- Assets/Scripts/Territory.cs
+++ Assets/Scripts/Territory.cs
@@ -10,6 +10,8 @@ public class Territory : MonoBehaviour
     [SerializeField] private RectTransform treeSpiritRect;
     [SerializeField] private RectTransform lavaSpiritRect;
 
+    [SerializeField] private LeanTweenType buttonEase;
+
     public float greenMaximum = 0;
     public float redMaximum = 0;
 
@@ -21,15 +23,44 @@ public class Territory : MonoBehaviour
         // Set each territory to it's minimum value
         redProgressBar.fillAmount = territoryMin;
         greenProgressBar.fillAmount = territoryMin;
+
+        // Set buttons to initial size
+        treeSpiritRect.localScale = new Vector3(0.5f, 0.5f, 0);
+        lavaSpiritRect.localScale = new Vector3(0.5f, 0.5f, 0);
+    }
+
+    private void Update()
+    {
+        
     }
 
     public void TreeSpiritPressed()
     {
+        var scaleX = treeSpiritRect.localScale.x;
+        var scaleY = treeSpiritRect.localScale.y;
+        Vector3 newScale = new Vector3(scaleX + 0.05f, scaleY + 0.05f, 0);
 
+        if (scaleX <= 1)
+        {
+            treeSpiritRect.LeanScale(newScale, 0.8f).setEase(buttonEase);
+        }
+        
+        for(int i = 0; i > 100; i++)
+        {
+            
+        }
+        greenProgressBar.fillAmount = Mathf.Lerp(greenProgressBar.fillAmount, 1f, 0.01f);
     }
 
     public void LavaSpiritPressed()
     {
-
+        var scaleX = lavaSpiritRect.localScale.x;
+        var scaleY = lavaSpiritRect.localScale.y;
+        Vector3 newScale = new Vector3(scaleX + 0.05f, scaleY + 0.05f, 0);
+        
+        if (scaleX <= 1)
+        {
+            lavaSpiritRect.LeanScale(newScale, 0.8f).setEase(buttonEase);
+        }
     }
 }
